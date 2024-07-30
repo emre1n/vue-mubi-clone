ff275d2e31cf67989ac2254de2577afd14df12fa
emre1n <emre1n@protonmail.com>
2024-07-30 21:43:30 +0300

    update the quality, allow the highest and one level lower than the highest

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -40,7 +40,10 @@ onMounted(() => {
   player.on('qualityChangeRendered', e => {
     // Assuming the highest quality is the last in the list
     const qualities = player.getBitrateInfoListFor('video');
-    if (e.newQuality === qualities.length - 1) {
+    if (
+      e.newQuality === qualities.length - 1 ||
+      e.newQuality === qualities.length - 2
+    ) {
       isHighQuality.value = true;
     }
   });
